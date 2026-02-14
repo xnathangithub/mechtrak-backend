ee4ec2d1cd46b909d08ed6891d2633528587d825
Your Name <you@example.com>
2026-02-13 21:53:59 -0500

    railway

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 // Test database connection
